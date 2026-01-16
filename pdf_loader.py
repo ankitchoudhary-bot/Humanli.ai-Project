defe871a0780d548ebfc9fc9a7d6ee20a26b8b34
Ankit954949 <ankit@mobiusdtaas.ai>
2026-01-16 20:41:08 +0530

    4th

--- pdf_loader.py
+++ pdf_loader.py
@@ -1,9 +1,6 @@
 from langchain_community.document_loaders import PyPDFLoader
 import spacy
 from langchain_classic.schema import Document
-# Load spaCy model
-
-import spacy
 from langchain_core.documents import Document
 
 _nlp = None
@@ -14,42 +11,6 @@ def get_nlp():
         _nlp = spacy.load("en_core_web_sm")
     return _nlp
 
-
-# def semantic_spacy_chunking(docs, max_chars=500):
-#     """
-#     Semantic chunking using spaCy sentence boundaries.
-#     Preserves metadata and avoids mid-sentence splits.
-#     """
-#     chunks = []
-
-#     for doc in docs:
-#         sentences = [sent.text.strip() for sent in nlp(doc.page_content).sents]
-
-#         current_chunk = ""
-#         for sent in sentences:
-#             # If adding sentence stays within limit → append
-#             if len(current_chunk) + len(sent) <= max_chars:
-#                 current_chunk += " " + sent
-#             else:
-#                 # Save chunk
-#                 chunks.append(
-#                     Document(
-#                         page_content=current_chunk.strip(),
-#                         metadata=doc.metadata
-#                     )
-#                 )
-#                 current_chunk = sent
-
-#         # Add leftover text
-#         if current_chunk:
-#             chunks.append(
-#                 Document(
-#                     page_content=current_chunk.strip(),
-#                     metadata=doc.metadata
-#                 )
-#             )
-
-#     return chunks
 def semantic_spacy_chunking(docs, max_chars=500):
     nlp = get_nlp()
     chunks = []
